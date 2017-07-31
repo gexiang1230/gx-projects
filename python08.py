0e971e38fcf6d74abb19061e3128e7477b4be0d8
unityka <2198645229@qq.com>
2017-07-31 14:47:56 +0800

    下载附件0008.zip（见附件0008.zip）
    解压该压缩包，里面 包含了3个文件。 都是数据库中导出的数据文件，
    记录了松勤的老师和授课信息。
    
    其中
    
    1. teacher.txt 文件中包含了 松勤的老师信息，格式如下：
    
    id;user_id;desc;display_idx;realname
    42;106;;1;小猪老师
    43;107;;2;mandy老师
    
    首行表示 下面数据各字段的含义。
    第1列id 表示老师的id号。
    第2、3、4 列的含义大家可以先不必了解。
    第5列realname 表示 老师的姓名
    
    2. course.txt 文件中包含了 松勤的课程信息，格式如下：
    
    id;name;desc;display_idx
    32;软件测试框架课程;;1
    33;web测试技术课程;;2
    
    首行表示 下面数据各字段的含义。
    第1列id 表示课程的id号
    第2列name 表示 课程的名称
    第3、4 列的含义大家可以先不必了解。
    
    3. teacher_course.txt 文件中包含了 松勤老师授课信息，格式如下：
    
    teacher_id;course_id
    42;32
    42;33
    
    首行表示 下面数据各字段的含义
    第1列teacher_id 表示老师的id号（对应第一张表的老师id号）
    第2列course_id表示 课程的id号（对应第二张表的课程id号）。
    
    要求大家编写一个python程序，根据这3张表的内容，输出一个包含老师授课信息的文件。
    要求格式为 老师姓名: 课程名，例如
    
    小猪老师    :  软件测试框架课程
    mandy老师   :  软件测试框架课程
    mandy老师   :  web测试技术课程
    
    实现过程中，有什么问题，请通过课堂上讲解的调试方法，尽量自己发现错误原因。

---
+++ python08.py
@@ -0,0 +1,41 @@
+# coding=utf-8#
+file1='C:\study\\0008\\teacher.txt'
+file2='C:\study\\0008\course.txt'
+file3='C:\study\\0008\\teacher_course.txt'
+file4='C:\study\\0008\put.txt'
+with open(file1) as f1,open (file2) as f2,open(file3) as f3:
+    teacher=f1.read().decode('utf-8').splitlines()[1:]#从文件1的第二行开始读取
+    course=f2.read().decode('utf-8').splitlines()[1:]
+    course_teacher=f3.read().decode('utf-8').splitlines()[1:]
+    teacherdict={}           #定义一个存储教师信息的空字典
+    for tea in teacher:
+        if not tea.strip():#strip()函数的目的是去除字符串中的空白字符
+            continue
+        teapart=tea.split(';')
+        teaid=teapart[0]
+        teaname=teapart[3]
+        teacherdict[teaid]=teaname
+
+    coursedict={}
+    for cou in course:
+        if not cou.strip( ):#strip()函数的目的是去除字符串中的空白字符
+            continue
+        coursepart=cou.split(';')
+        courseid=coursepart[0]
+        coursename=coursepart[1]
+        coursedict[courseid]=coursename
+    #print coursedict
+
+with open(file4,'w') as f4:
+    for tea_cor in course_teacher:
+        if not  tea_cor.strip():
+          continue
+        tea_co=tea_cor.split(';')
+        teaid_cor_part=tea_co[0]
+        tea_corid_part = tea_co[1]
+        if (teaid_cor_part not in teacherdict )or (tea_corid_part not in coursedict):
+         print 'skip record{}'.format(tea_cor)
+
+        ret=u"{:10}:{}".format(teacherdict[teaid_cor_part],coursedict[tea_corid_part])
+        print ret
+        f4.write(ret.encode('utf-8')+'\n')
